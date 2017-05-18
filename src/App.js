1e1e43e472fd9030556102b6c6fbe90dbf37d2f7
Jeffrey Fisher <jeff@slofish.io>
2017-05-17 19:39:36 -0700

    Added open/closed switch. Fixed bug where it displayed all issues, not just open ones. Major styling changes.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       totalCount: '',
       sortDesc: true,
       sortType: 'updated',
+      issueOpen: true,
       issues: [],
       labelValues: '',
       keywordValues: ''
@@ -19,12 +20,13 @@ class App extends Component {
 
     this.toggleSortType = this.toggleSortType.bind(this);
     this.toggleSortOrder = this.toggleSortOrder.bind(this);
+    this.toggleIssueOpen = this.toggleIssueOpen.bind(this);
     this.initiateAPICall = this.initiateAPICall.bind(this);
     this.handleFormChange = this.handleFormChange.bind(this);
     this.handleInputBlur = this.handleInputBlur.bind(this);
   }
 
-  initiateAPICall(sortDesc, sortType, labelValues, keywordValues) {
+  initiateAPICall(sortDesc, sortType, labelValues, keywordValues, issueOpen) {
     let myHeaders = new Headers({
       Authorization: 'token ' + config.apiToken
     });
@@ -62,7 +64,8 @@ class App extends Component {
     }
     let resultsPerPage = 25;
     let sortOrder = sortDesc ? 'desc' : 'asc';
-    let searchQuery = keywordQuery + maybePlus + labelQuery + '&per_page=' + resultsPerPage + '&type=issue&state=open&page=1&sort=' + sortType + '&order=' + sortOrder
+    let issueState = issueOpen ? 'open' : 'closed';
+    let searchQuery = keywordQuery + maybePlus + labelQuery + '+type:issue+state:' + issueState + '&page=1&sort=' + sortType + '&order=' + sortOrder + '&per_page=' + resultsPerPage;
     let myRequest = new Request('https://api.github.com/search/issues?q=' + searchQuery);
 
     let self = this;
@@ -74,7 +77,8 @@ class App extends Component {
         issues: data.items,
         sortDesc: sortDesc,
         sortType: sortType,
-        labelValues: labelValues
+        labelValues: labelValues,
+        issueOpen: issueOpen
       });
     });
   }
@@ -86,46 +90,54 @@ class App extends Component {
           <h1>Open Source Help Wanted</h1>
           <h2>Find issues you can work on in Github. Be a contributor!</h2>
         </div>
-
-        <div className="app-body">
-          <div className="App-intro">
-            <p className="total-count">Displaying {this.state.issues.length} of {this.state.totalCount} issues.</p>
-
-            <form>
-              <div className="input-elements">
-                <div className="label-search-box">
-                  <label className="label-name">Github label names</label>
-                  <input className="input-element labelSearch" name="labelValues" type="text" placeholder="help wanted, bug"
-                         onKeyPress={this.handleFormChange} onBlur={this.handleInputBlur}/>
-                  <label className="label-keywords">Keywords</label>
-                  <input className="input-element labelKeywords" name="keywordValues" type="text" placeholder="open source, forms"
+        <div className="App-intro">
+          <form>
+            <div className="input-elements">
+              <div className="label-search-box">
+                <div className="input-component">
+                <label className="label-name">Github label names</label>
+                  <input className="input-element" name="labelValues" type="text"
+                         placeholder="help wanted, bug"
                          onKeyPress={this.handleFormChange} onBlur={this.handleInputBlur}/>
                 </div>
-
-                <label className="sorting-options">Sorting Options</label>
-                <div className="sort-inputs">
-                  <div className="input-element">
-                    sort by created time
-                    <Switch on={this.state.sortType === 'updated'} onClick={this.toggleSortType}/>
-                    sorty by updated time
-                  </div>
-
-                  <div className="input-element">
-                    Oldest first
-                    <Switch on={this.state.sortDesc} onClick={this.toggleSortOrder}/>
-                    Newest first
-                  </div>
+                <div className="input-component">
+                  <label className="label-name">Keywords</label>
+                  <input className="input-element" name="keywordValues" type="text"
+                        placeholder="open source, forms"
+                         onKeyPress={this.handleFormChange} onBlur={this.handleInputBlur}/>
                 </div>
               </div>
-            </form>
-          </div>
 
+              <div className="option-inputs">
+                <div className="input-element">
+                  <label>Sort by created time</label>
+                  <Switch on={this.state.sortType === 'updated'} onClick={this.toggleSortType}/>
+                  <label>Sort by updated time</label>
+                </div>
+
+                <div className="input-element">
+                  <label>Oldest first</label>
+                  <Switch on={this.state.sortDesc} onClick={this.toggleSortOrder}/>
+                  <label>Newest first</label>
+                </div>
+
+                <div className="input-element">
+                  <label>Closed</label>
+                  <Switch on={this.state.issueOpen} onClick={this.toggleIssueOpen}/>
+                  <label>Open</label>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="app-body">
+          <p className="total-count">Displaying {this.state.issues.length} of {this.state.totalCount ? this.state.totalCount : 0} issues.</p>
           <div className="app-results">
             <Issues data={this.state.issues}/>
           </div>
         </div>
         <footer>
-          <p><a href="https://github.com/jeffslofish/open-source-help-wanted">Fork me on Github!</a></p>
+          <p><a href="https://github.com/jeffslofish/open-source-help-wanted">Fork me on Github and contribute!</a></p>
         </footer>
       </div>
     );
@@ -137,11 +149,15 @@ class App extends Component {
     if (this.state.sortType === 'updated') {
       sortType = 'created';
     }
-    this.initiateAPICall(this.state.sortDesc, sortType, this.state.labelValues, this.state.keywordValues);
+    this.initiateAPICall(this.state.sortDesc, sortType, this.state.labelValues, this.state.keywordValues, this.state.issueOpen);
   }
 
   toggleSortOrder() {
-    this.initiateAPICall(!this.state.sortDesc, this.state.sortType, this.state.labelValues, this.state.keywordValues);
+    this.initiateAPICall(!this.state.sortDesc, this.state.sortType, this.state.labelValues, this.state.keywordValues, this.state.issueOpen);
+  }
+
+  toggleIssueOpen() {
+    this.initiateAPICall(this.state.sortDesc, this.state.sortType, this.state.labelValues, this.state.keywordValues, !this.state.issueOpen);
   }
 
   handleFormChange(e) {
@@ -150,7 +166,8 @@ class App extends Component {
         this.state.sortDesc,
         this.state.sortType,
         (e.target.name === 'labelValues') ? e.target.value : this.state.labelValues,
-        (e.target.name === 'keywordValues') ? e.target.value : this.state.keywordValues);
+        (e.target.name === 'keywordValues') ? e.target.value : this.state.keywordValues,
+        this.state.issueOpen);
     }
   }
 
