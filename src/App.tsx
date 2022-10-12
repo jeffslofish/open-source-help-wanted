458f047bb5261cf2fa109962dd9409f36a302124
Virus <coderelay2020@gmail.com>
2022-10-12 22:23:21 +0530

    added some seo friendly meta tags to website (#74)
    Closes #46

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Helmet from "react-helmet";
 import ReactGA from 'react-ga4';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
@@ -11,6 +12,26 @@ ReactGA.send('pageview');
 const App = () => {
   return (
     <Router>
+    <Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Open Source Help Wanted" />
+        <meta name="twitter:title" content="Open Source Help Wanted" />
+
+
+        <meta name="description" content="Search GitHub for open source issues to work on" />
+        <meta name="og:description" content="Search GitHub for open source issues to work on" />
+        <meta name="twitter:description" content="Search GitHub for open source issues to work on" />
+
+        <meta name="url" content="https://www.opensourcehelpwanted.com/" />
+        <meta name="og:url" content="https://www.opensourcehelpwanted.com/" />
+        <meta name="twitter:url" content="https://www.opensourcehelpwanted.com/" />
+
+        <meta name="keywords" content="open source projects, github, open source issues" />
+
+        <link rel="canonical" href="https://www.opensourcehelpwanted.com/" />
+
+
+      </Helmet>
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/repos' element={<ReposHomePage />}></Route>
